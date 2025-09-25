5cdda5820f75e24a668f9c21ddcb22fbfb382dde
Dulakshi <dulakshi.thariduka@innerspace.eu>
2025-09-25 14:52:58 +0200

    fix login test

--- tests/proj-management.spec.ts
+++ tests/proj-management.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
-import { LoginPage, CustomerPage, ProjectPage, ProcessPage } from '../pages'; 
+import { LoginPage, CustomerPage, ProjectPage, ProcessPage } from '../pages';
 
 test.describe.configure({ mode: 'serial' });
 
@@ -8,47 +8,49 @@ const customerName = faker.company.name();
 const projectName  = faker.commerce.productName();
 const processName  = `${faker.word.noun()}-${faker.number.int(999)}`;
 
-test('Login and Navigate', async ({ page }) => {
-  const email = process.env.USER_EMAIL;
-  const password = process.env.USER_PASSWORD;
-  if (!email || !password) {
-    throw new Error('Missing USER_EMAIL or USER_PASSWORD in .env');
-  }
+let page;
+
+test.beforeAll(async ({ browser }) => {
+  const context = await browser.newContext();
+  page = await context.newPage();
+
+  const email = process.env.USER_EMAIL!;
+  const password = process.env.USER_PASSWORD!;
   const login = new LoginPage(page);
+
   await login.open();
   await login.signIn(email, password);
-  //await login.expectLoggedIn();
 });
 
-test('Create Customer', async ({ page }) => {
+test('Create Customer', async () => {
   const customerPage = new CustomerPage(page);
-  await customerPage.goToCustomersTab();
+  //await customerPage.goToCustomersTab();
   await customerPage.createCustomer(customerName, 'Sample Desc');
 });
 
-test('Create Project', async ({ page }) => {
+test('Create Project', async () => {
   const projectPage = new ProjectPage(page);
   await projectPage.goToProjectsTab();
   await projectPage.createProject(projectName, customerName);
 });
 
-test('Select Project', async ({ page }) => {
+test('Select Project', async () => {
   const projectPage = new ProjectPage(page);
   await projectPage.filterByProject(projectName);
 });
 
-test('Create Process', async ({ page }) => {
+test('Create Process', async () => {
   const processPage = new ProcessPage(page);
   await processPage.goToProcessesTab();
   await processPage.createProcess(processName);
 });
 
-test('Select Process', async ({ page }) => {
+test('Select Process', async () => {
   const processPage = new ProcessPage(page);
   await processPage.selectProcessInProject(processName);
 });
 
-test('Verify Created Process and Project', async ({ page }) => {
+test('Verify Created Process and Project', async () => {
   const processPage = new ProcessPage(page);
   await processPage.expectSidebarHas(projectName, processName);
   await processPage.openFrameAssignmentAndExpect();
